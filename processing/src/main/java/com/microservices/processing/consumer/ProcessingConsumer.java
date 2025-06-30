0c88efd0073e12f644f3101be71a91874d8c0c67
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-06-30 16:00:52 -0300

    update(ProcessingConsumer): Using OrderDTO in method listener

--- processing/src/main/java/com/microservices/processing/consumer/ProcessingConsumer.java
+++ processing/src/main/java/com/microservices/processing/consumer/ProcessingConsumer.java
@@ -1,14 +1,16 @@
 package com.microservices.processing.consumer;
 
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import com.microservices.processing.dto.OrderDTO;
 
 @Component
 public class ProcessingConsumer {
 
 	@RabbitListener(queues = "${broker.queue.processamento.name}")
-	public void listenerProcessingQueue(@Payload String description) {
-		System.out.println(description);
+	public void listenerProcessingQueue(OrderDTO orderDTO) {
+		System.out.println(orderDTO.getDescription());
 	}
 }
